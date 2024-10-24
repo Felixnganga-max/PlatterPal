a04a2979520aa88d5e1ca5f6d5db521b42467622
FELIX NGUNGA <66121374+Felixnganga-max@users.noreply.github.com>
2024-10-24 20:12:18 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,38 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from './routes/foodRoute.js'
-import 'dotenv/config'
-import userRouter from "./routes/userRouter.js"
-import cartRouter from "./routes/cartRoutes.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from './routes/foodRoute.js';
+import 'dotenv/config';
+import userRouter from "./routes/userRouter.js";
+import cartRouter from "./routes/cartRoutes.js";
+import orderRouter from "./routes/orderRoute.js";
 
-const port = 4000 || process.env.PORT;
-
+const port = process.env.PORT || 4000;
 
 // App config
-const app = express()
+const app = express();
 
-
-// middleware
-app.use(express.json())
+// Middleware
+app.use(express.json());
 app.use(cors({
   origin: 'https://platterpal-qliz.onrender.com'  // Frontend origin
 }));
 
-
-
-// db connection 
+// DB connection 
 connectDB();
 
-//API ENDPOINT
-app.use("/api/food/", foodRouter)
-app.use("/images", express.static("uploads"))
-app.use("/api/user", userRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
+// API Endpoints
+app.use("/api/food/", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.get("/", (req, res) =>{
-    res.send("API Working")
-})
-
-app.listen(port, ()=>{
-    console.log(`Server running on http://localhost:${port}`)
-})
-
-
-
-// mongodb+srv://standardwebtechnologies:<db_password>@cluster0.evei3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
