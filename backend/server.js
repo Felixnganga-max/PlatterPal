dfa75dfa1b1c018c0cb22e8ebf1df695a5ccf7de
FELIX NGUNGA <66121374+Felixnganga-max@users.noreply.github.com>
2024-11-18 23:24:00 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: [
-    'https://platter-pal-frondend.vercel.app/'
+    'https://platter-pal-frondend.vercel.app'
   ]
 }));
 
